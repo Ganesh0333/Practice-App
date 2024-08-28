f0b93e4fc38c0973eacbdc9f43e3792221ff89fd
Ganesh0333 <kottaliganesh1994@gmail.com>
2024-08-28 13:52:56 +0530

    Login & Dashboard Created and its paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"LoginComponent",component:LoginComponent},
+  {path:"dashboard",component:DashboardComponent},
+  {path:"",component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
